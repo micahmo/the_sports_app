4eed60f25a16d3981e4084a82190508716ed1d6f
Micah Morrison <micahmorrison95@gmail.com>
2026-09-20 23:00:47 -0400

    Fix app title and description

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,18 @@ import 'src/theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await loadThemeMode();
-  runApp(const StreamedApp());
+  runApp(const SportsApp());
 }
 
-class StreamedApp extends StatelessWidget {
-  const StreamedApp({super.key});
+class SportsApp extends StatelessWidget {
+  const SportsApp({super.key});
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<ThemeMode>(
       valueListenable: themeModeNotifier,
       builder: (BuildContext context, ThemeMode mode, Widget? _) {
         return MaterialApp(
-          title: 'Streamed',
+          title: 'sports',
           theme: buildLightTheme(),
           darkTheme: buildDarkTheme(),
           themeMode: mode,
